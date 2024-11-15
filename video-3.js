c31763028ea8ad5b797e81d97c862d22f4adeb4e
Pratap Tondwalkar <pratap.tondwalkar@gmail.com>
2024-11-15 19:51:59 +0530

    updated content for Video 3

---
+++ video-3.js
@@ -0,0 +1,10 @@
+console.log([2] == [2])
+
+// In JavaScript Array is nothing but an Object
+// Object is reference Type
+
+// [2] in JS memory it will be referred as RF1
+// Other [2] in JS memory it will be referred as RF2
+
+
+// RF1 == RF2 will return false.
